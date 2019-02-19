30c48c8878a504472a8690b7273b3c892145c21c
Damien Guard <damieng@gmail.com>
2019-02-18 21:14:03 -0800

    set-my-metadata now names Italics.

--- FontLab5Macros/set-my-metadata.py
+++ FontLab5Macros/set-my-metadata.py
@@ -6,29 +6,42 @@ font = fl.font
 
 # Base name on filename
 fontName = os.path.basename(os.path.splitext(font.file_name)[0]).strip()
-isBold = fontName.endswith(" Bold")
+nameParts = fontName.split()
+isBold = True if "Bold" in nameParts else False
+isItalic = True if "Italic" in nameParts else False
+
+styleNames = []
+font.font_style = 0
+font.weight = "Regular"
+font.weight_code = 400
+
 if isBold:
-    familyName = fontName[0:-5]
-    styleName = "Bold"
-    font.font_style = 32
+    styleNames.append("Bold")
+    nameParts.remove("Bold")
+    font.font_style += 32
     font.weight = "Bold"
     font.weight_code = 700
-else:
-    familyName = fontName
-    styleName = "Regular"
-    font.font_style = 0
-    font.weight = "Regular"
-    font.weight_code = 400
 
+if isItalic:
+    styleNames.append("Italic")
+    nameParts.remove("Italic")
+    font.font_style += 1
+
+if styleNames:
+    styleName = " ".join(styleNames)
+else:
+    styleName = "Regular"
+
+familyName = " ".join(nameParts)
 fullName = familyName + " " + styleName
 
 # Set all standard names
 font.family_name = familyName
 font.style_name = styleName
-font.font_name = familyName.replace(' ','') + "-" + styleName
+font.font_name = familyName.replace(' ', '') + "-" + styleName.replace(' ', '')
 font.full_name = fullName
 font.menu_name = familyName
-font.apple_name = fullName if isBold else familyName
+font.apple_name = fullName if isBold or isItalic else familyName
 
 # Personalized creator info
 font.designer = "Damien Guard"
